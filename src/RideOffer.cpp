9a5ef398133b84cc323f66aa1e500b479e9bc342
Andre <andrebrreis@gmail.com>
2016-04-24 18:41:26 +0100

    Add route list stub

--- src/RideOffer.cpp
+++ src/RideOffer.cpp
@@ -2,7 +2,7 @@
  * RideOffer.cpp
  *
  *  Created on: 20/04/2016
- *      Author: Inês
+ *      Author: Ines
  */
 
 #include "RideOffer.h"
@@ -10,6 +10,9 @@
 RideOffer::RideOffer(time_t departureTime, time_t estimatedArrival, time_t departureTolerance, time_t arrivalTolerance, int noSeats, User* driver):
 	Ride(departureTime, estimatedArrival, departureTolerance, arrivalTolerance, noSeats), driver(driver){
 	// TODO Auto-generated constructor stub
+    // TODO: Uncomment when points are implemented
+	//route.push_back(departurePoint);
+    //route.push_back(arrivalPoint);
 
 }
 
--- src/RideOffer.h
+++ src/RideOffer.h
@@ -2,17 +2,21 @@
  * RideOffer.h
  *
  *  Created on: 20/04/2016
- *      Author: Inês
+ *      Author: Ines
  */
 
 #ifndef SRC_RIDEOFFER_H_
 #define SRC_RIDEOFFER_H_
 
 #include "Ride.h"
+#include "Crossroad.h"
+#include "Graph.h"
+#include <list>
 
 class RideOffer: public Ride {
 private:
 	User* driver;
+	std::list<Vertex<Crossroad> > route;
 public:
 	RideOffer(time_t departureTime, time_t estimatedArrival, time_t departureTolerance, time_t arrivalTolerance, int noSeats, User* driver);
 	virtual ~RideOffer();
